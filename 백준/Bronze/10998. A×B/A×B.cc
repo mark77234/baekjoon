5d09e7d48783868f60c475e63719454ca21b0e5e
mark77234 <114278878+mark77234@users.noreply.github.com>
2024-04-08 10:46:37 +0900

    [Bronze V] Title: A×B, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/10998. A×B/A×B.cc
@@ -0,0 +1,12 @@
+#include <iostream>
+
+/* run this program using the console pauser or add your own getch, system("pause") or input loop */
+
+int main(int argc, char** argv) {
+	int A , B, C;
+	scanf("%d %d", &A, &B);
+	C = A * B;
+	
+	printf("%d", C);
+	return 0;
+}
