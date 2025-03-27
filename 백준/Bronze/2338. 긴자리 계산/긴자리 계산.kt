ec6922b7c8ac61c0296f240f439f14971f17bee9
병찬 <114278878+mark77234@users.noreply.github.com>
2025-03-27 11:17:03 +0900

    [Bronze V] Title: 긴자리 계산, Time: 124 ms, Memory: 14076 KB -BaekjoonHub

---
+++ 백준/Bronze/2338. 긴자리 계산/긴자리 계산.kt
@@ -0,0 +1,13 @@
+import java.math.BigInteger
+import java.util.Scanner
+
+fun main() {
+    val sc = Scanner(System.`in`)
+    val a = sc.nextBigInteger()
+    val b = sc.nextBigInteger()
+
+    println(a+b)
+    println(a-b)
+    println(a*b)
+
+}
